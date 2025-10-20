8ac9e372aa4d078e86b9d51649db126b348360fc
parker <parkersfly@gmail.com>
2025-10-20 19:43:02 -0300

    feat: add the function that creates a new task

--- server/app/Http/Controllers/API/TaskController.php
+++ server/app/Http/Controllers/API/TaskController.php
@@ -7,5 +7,10 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    // Cria uma nova tarefa
+    public function store(TaskRequest $request)
+    {
+        $task = Task::create($request->validated());
+        return response()->json($task, 201);
+    }
 }
