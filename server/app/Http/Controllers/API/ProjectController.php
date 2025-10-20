ad67bf6ecd6355d0cfd3c18b4836d3dbcb6ac431
Adrian Bezerra Pires Martins <adrian.martins@cnj.jus.br>
2025-10-20 16:58:42 -0300

    feat: create function to list all projects

--- server/app/Http/Controllers/API/ProjectController.php
+++ server/app/Http/Controllers/API/ProjectController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProjectRequest;
+use App\Models\Project;
 use Illuminate\Http\Request;
+
 
 class ProjectController extends Controller
 {
-    //
+    // Lista todos os projetos
+    public function index()
+    {
+         $projects = Project::with('tasks')->get();
+         return response()->json($projects);
+    }
 }
