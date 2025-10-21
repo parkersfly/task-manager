99cf4367b1c9f342297b5a01fd5f94b4c40d399a
parker <parkersfly@gmail.com>
2025-10-20 23:19:21 -0300

    feat: create component TaskForm

---
+++ web/src/components/TaskForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form @submit.prevent="addNewTask" class="d-flex gap-2">
+    <input v-model="title" type="text" class="form-control" placeholder="Nova tarefa..." required />
+    <button class="btn btn-success">Adicionar</button>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useTaskStore } from '../stores/useTaskStore'
+
+const store = useTaskStore()
+const title = ref('')
+
+async function addNewTask() {
+  await store.addTask(title.value)
+  title.value = ''
+}
+</script>
