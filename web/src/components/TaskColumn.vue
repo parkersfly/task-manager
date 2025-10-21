b2d97997ba8efb89715ca1cab8bcc5a8dbeaf795
Adrian Bezerra Pires Martins <adrian.martins@cnj.jus.br>
2025-10-21 09:09:05 -0300

    feat: add compontent TaskColumn

---
+++ web/src/components/TaskColumn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="col-md-4">
+    <div class="card shadow-sm mb-3">
+      <div class="card-header bg-primary text-white">
+        {{ title }}
+      </div>
+      <div 
+      class="card-body"
+      style="min-height: 100px;"
+      >
+        <div v-if="tasks.length === 0" class="text-muted small">Nenhuma tarefa.</div>
+        <TaskCard 
+        v-for="task in tasks" 
+        :key="task.id" 
+        :task="task"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TaskCard from './TaskCard.vue'
+
+const props = defineProps({
+  title: String,
+  tasks: Array,
+})
+</script>
